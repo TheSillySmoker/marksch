610fca00cc006384924ee40328853c79d6443c38
thesillysmoker <thesillysmoker@gmail.com>
2025-06-21 14:55:44 +1000

    c

--- create.sh
+++ create.sh
@@ -8,15 +8,15 @@ echo "Making a new folder: $variant/$article"
 mkdir $variant/$article
 
 #make blank article in the new directory
-echo "Making copy of blog/blank.html and putting it in your new folder"
+echo "Making copy of blog/blank.html and putting it in $variant/$article."
 cp blog/blank.html $variant/$article/
 
 #rename the blank article
-echo "Renaming your new blank article"
+echo "Renaming your new blank article to $article.html \n\n"
 mv $variant/$article/blank.html $variant/$article/$article.html
 
 #Add it into the home page as a button if you give it a valid topic
-echo "Please enter the topic (finance, sd, misc)"
+echo "Please enter the topic (finance, sd, misc):"
 read topic
 
 line=0
@@ -53,4 +53,4 @@ else
 	echo "No topic found, no button will be added :)"
 fi
 
-echo "Your new article has been created. In $variant/$article. "
+echo "Your new article has been created in $variant/$article. \n and a new button has been create in home.html on line $line"
