7594a9bc0d6adc1ad9682bbcd979457d68be8a03
thesillysmoker <thesillysmoker@gmail.com>
2026-01-11 16:26:01 +1000

    c

--- update_portfolio.sh
+++ update_portfolio.sh
@@ -5,12 +5,7 @@
 set -euo pipefail
 
 rm blog/portfolio/TSLA_purchase_history/TSLA_purchases.png
-echo "Adding files"
-git add --all
-echo "Committing files"
-git commit -a -m "c"
-echo "Pushing to GitHub"
-git push
+../code/bashScripts/updateMarksch.sh
 #git rm blog/portfolio/TSLA_purchase_history/TSLA_purchases.png
 #git rm --cached blog/portfolio/tsla_purchase_history/tsla_purchases.png
 python3 blog/portfolio/TSLA_purchase_history/date_plot_chart3.py
