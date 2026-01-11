f06be583d91a99d47bfae0cecb1f978e51057cff
thesillysmoker <thesillysmoker@gmail.com>
2026-01-11 15:33:20 +1000

    c

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@
 cd ~/Documents/marksch
 
 git add --all
-git add blog/portfolio/TSLA_purchase_history/TSLA_purchases.png
+git add -f blog/portfolio/TSLA_purchase_history/TSLA_purchases.png
 git commit -a -m "c"
 git push
