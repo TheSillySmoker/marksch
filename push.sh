cbc1e16a07f0db93678232bd0fdbed18c78a5d5d
thesillysmoker <thesillysmoker@gmail.com>
2026-01-11 15:37:40 +1000

    c

--- push.sh
+++ push.sh
@@ -2,7 +2,11 @@
 
 cd ~/Documents/marksch
 
-git add --all
+echo "Adding files"
+git rm --cached blog/portfolio/TSLA_purchase_history/TSLA_purchases.png
 git add -f blog/portfolio/TSLA_purchase_history/TSLA_purchases.png
+git add --all
+echo "Committing files"
 git commit -a -m "c"
+echo "Pushing to GitHub"
 git push
