6fd8eb03622d3d75ced4ba2e2c5627dd1f059a17
thesillysmoker <thesillysmoker@gmail.com>
2026-08-08 15:28:24 +1000

    c

---
+++ create2.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "Please enter the name of the new article (this will be the name of the folder created): "
+read article
+echo "Enter blog or home"
+read variant 
+echo "Button: <a href="$variant/$article/$article.html"><button class="button"><b>$button_name</b></button></a>"
+
